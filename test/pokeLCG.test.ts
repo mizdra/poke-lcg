9bb5419b1ac36113317852f5dc4ae38f833cf7e1
mizdra <pp.mizdra@gmail.com>
2016-04-01 14:27:08 +0900

    Fix test

--- test/pokeLCG.test.ts
+++ test/pokeLCG.test.ts
@@ -7,66 +7,49 @@ import * as assert from 'power-assert';
 
 describe('generator', () => {
     it('generator#next()', () => {
-        const g = generator(GEN3_ARG, 0x00000000);
+        const g1 = generator(GEN3_ARG, 0x00000000);
+        const g2 = generator(GEN3_ARG, 0x00000000, 0);
+        const g3 = generator(GEN3_ARG, 0x00000000, 1);
 
-        assert.deepEqual(g.next(), {value: 0x00006073, done: false});
-        assert.deepEqual(g.next(), {value: 0xe97e7b6a, done: false});
-        assert.deepEqual(g.next(), {value: 0x52713895, done: false});
+        assert.deepEqual(g1.next(), {value: 0x00006073, done: false});
+        assert.deepEqual(g1.next(), {value: 0xe97e7b6a, done: false});
+        assert.deepEqual(g1.next(), {value: 0x52713895, done: false});
+        g1.return();
+        assert.deepEqual(g1.next(), {value: undefined , done: true});
+        assert.deepEqual(g2.next(), {value: undefined , done: true});
+        assert.deepEqual(g3.next(), {value: 0x00006073, done: false});
+        assert.deepEqual(g3.next(), {value: undefined , done: true});
     });
 
-    it('generator#next(frame: number)', () => {
-        const g = generator(GEN3_ARG, 0x00000000);
+    it('generator#next(frame: any)', () => {
+        const g1 = generator(GEN3_ARG, 0x00000000);
+        const g2 = generator(GEN3_ARG, 0x00000000);
+        const g3 = generator(GEN3_ARG, 0x00000000, 1);
 
-        assert.deepEqual(g.next(1), {value: 0x00006073, done: false});
-        assert.deepEqual(g.next(2), {value: 0x52713895, done: false});
-        assert.deepEqual(g.next(4), {value: 0xafc58ac9, done: false});
+        assert.deepEqual(g1.next(1), {value: 0x00006073, done: false});
+        assert.deepEqual(g2.next(2), {value: 0xe97e7b6a, done: false});
+        assert.deepEqual(g2.next(4), {value: 0xe2cca5ee, done: false});
+        assert.deepEqual(g2.next(8), {value: 0x993d6bb6, done: false});
+        assert.deepEqual(g3.next(2), {value: undefined , done: true});
     });
 
-    it('generator#return(value: number)', () => {
+    it('generator#return(value: any)', () => {
         const g = generator(GEN3_ARG, 0x00000000);
 
-        assert.deepEqual(g.return(1), {value: 1, done: true});
         assert.deepEqual(g.return(2), {value: 2, done: true});
         assert.deepEqual(g.return(4), {value: 4, done: true});
+        assert.deepEqual(g.return(8), {value: 8, done: true});
     });
 
     it('generator#throw(e: any)', () => {
         const g = generator(GEN3_ARG, 0x00000000);
 
-        try {
-            g.throw();
-        } catch (e) {
-            assert(e === undefined);
-        }
-
-        try {
-            g.throw(new Error());
-        } catch (e) {
-            assert(e.message === '');
-        }
-
-        try {
-            g.throw(new Error('message'));
-        } catch (e) {
-            assert(e.message === 'message');
-        }
-    });
-
-    it('done', () => {
-        const g1 = generator(GEN3_ARG, 0x00000000);
-        const g2 = generator(GEN3_ARG, 0x00000000, 0);
-        const g3 = generator(GEN3_ARG, 0x00000000, 1);
-        const g4 = generator(GEN3_ARG, 0x00000000, -1);
-
-        assert.deepEqual(g1.next(100), {value: 0x451bde44, done: false});
-        assert.deepEqual(g2.next()   , {value: undefined , done: true});
-        assert.deepEqual(g3.next()   , {value: 0x00006073, done: false});
-        assert.deepEqual(g3.next()   , {value: undefined , done: true});
-        assert.deepEqual(g4.next()   , {value: undefined , done: true});
+        assert.throws(() => g.throw(new Error()), '');
+        assert.throws(() => g.throw(new Error('message')), 'message');
     });
 });
 
-describe('ARG', () => {
+describe('LCGArg', () => {
     it('GEN3_ARG', () => {
         assert.deepEqual(GEN3_ARG, {multiplier: 0x41C64E6D, increment: 0x6073});
     });
