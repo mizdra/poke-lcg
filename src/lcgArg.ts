34c8e95e71527991fe478cf8b044ded63fc0ff70
mizdra <pp.mizdra@gmail.com>
2016-04-03 16:01:21 +0900

    Fix GEN4_ALTERNATIVE_ARG

--- src/lcgArg.ts
+++ src/lcgArg.ts
@@ -5,6 +5,6 @@ export interface LCGArg {
     increment: number;
 }
 
-export const GEN3_ARG: LCGArg             = Object.freeze({multiplier: 0x41C64E6D, increment: 0x6073});
-export const GEN4_ARG: LCGArg             = Object.freeze({multiplier: 0x41C64E6D, increment: 0x6073});
-export const GEN4_ALTERNATIVE_ARG: LCGArg = Object.freeze({multiplier: 0x41C64E6D, increment: 0x6073});
+export const GEN3_ARG: LCGArg             = Object.freeze({multiplier: 0x41C64E6D, increment: 0x00006073});
+export const GEN4_ARG: LCGArg             = Object.freeze({multiplier: 0x41C64E6D, increment: 0x00006073});
+export const GEN4_ALTERNATIVE_ARG: LCGArg = Object.freeze({multiplier: 0x6C078965, increment: 0x00000001});
