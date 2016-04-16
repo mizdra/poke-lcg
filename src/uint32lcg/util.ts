532c4201adc35965e56a1d6af3b915d9b77c16d9
mizdra <pp.mizdra@gmail.com>
2016-04-16 19:28:32 +0900

    Add JSDoc

--- src/uint32lcg/util.ts
+++ src/uint32lcg/util.ts
@@ -15,13 +15,17 @@ export function toRandom(seed: number): number {
 
 /**
  * Converts a number to a hex string.
+ * @example
+ * Uint32LCG.toHexString(0x12345678  , 8); // '12345678'
+ * Uint32LCG.toHexString(0xAB12345678, 8); // '12345678'
+ * Uint32LCG.toHexString(0x1234      , 8); // '00001234'
+ * Uint32LCG.toHexString(0x1234      , 2); // '34'
+ * Uint32LCG.toHexString(0x1234      , 0); // ''
  * @param seed      - A number
  * @param maxLength - The length of a hex string
  * @returns         - The hex string, which is specified length 
  */
 export function toHexString(num: number, maxLength: number): string {
     const hex = num.toString(16);
-
-    // Write comment
     return hex.padStart(maxLength, '0').substr(-maxLength, maxLength);
 }
