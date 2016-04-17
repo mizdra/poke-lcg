221c4a83de53dfc21b8675467a582043707ba56c
mizdra <pp.mizdra@gmail.com>
2016-04-17 15:18:44 +0900

    Add TypeDoc

--- src/uint32/lcgArg.ts
+++ src/uint32/lcgArg.ts
@@ -4,7 +4,14 @@
  * The interface of LCG argument
  */
 export interface ILCGArg {
+    /**
+     * The multiplier of LCG
+     */
     multiplier: number;
+
+    /**
+     * The increment of LCG
+     */
     increment: number;
 }
 
