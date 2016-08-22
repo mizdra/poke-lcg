7e0265ff2adf63ab535bf0aa7b5b1b8aa6839273
mizdra <pp.mizdra@gmail.com>
2016-08-22 22:25:02 +0900

    Cast seed to u32

--- test/uint32/generator.test.ts
+++ test/uint32/generator.test.ts
@@ -3,38 +3,29 @@
 import * as Uint32LCG from '../../uint32';
 import * as assert from 'power-assert';
 
-function u32(result: IteratorResult<number>): IteratorResult<number> {
-    let value: number = result.value;
-
-    return {
-        value: Number.isInteger(value) ? value >>> 0 : value,
-        done: result.done
-    };
-}
-
 describe('generator', () => {
     it('generator#next()', () => {
         const g1 = Uint32LCG.generator(Uint32LCG.GEN3_ARG, 0x00000000);
-        assert.deepEqual(u32(g1.next()), {value: 0x00006073, done: false});
-        assert.deepEqual(u32(g1.next()), {value: 0xe97e7b6a, done: false});
-        assert.deepEqual(u32(g1.next()), {value: 0x52713895, done: false});
+        assert.deepEqual(g1.next(), {value: 0x00006073, done: false});
+        assert.deepEqual(g1.next(), {value: 0xe97e7b6a, done: false});
+        assert.deepEqual(g1.next(), {value: 0x52713895, done: false});
         g1.return();
-        assert.deepEqual(u32(g1.next()), {value: undefined , done: true});
+        assert.deepEqual(g1.next(), {value: undefined , done: true});
 
         const g2 = Uint32LCG.generator(Uint32LCG.GEN3_ARG, 0x00000000, 0);
-        assert.deepEqual(u32(g2.next()), {value: undefined , done: true});
+        assert.deepEqual(g2.next(), {value: undefined , done: true});
 
         const g3 = Uint32LCG.generator(Uint32LCG.GEN3_ARG, 0x00000000, 1);
-        assert.deepEqual(u32(g3.next()), {value: 0x00006073, done: false});
-        assert.deepEqual(u32(g3.next()), {value: undefined , done: true});
+        assert.deepEqual(g3.next(), {value: 0x00006073, done: false});
+        assert.deepEqual(g3.next(), {value: undefined , done: true});
     });
 
     it('generator#return(value: any)', () => {
         const g = Uint32LCG.generator(Uint32LCG.GEN3_ARG, 0x00000000);
 
-        assert.deepEqual(u32(g.return(2)), {value: 2        , done: true});
-        assert.deepEqual(u32(g.return(4)), {value: undefined, done: true});
-        assert.deepEqual(u32(g.return(8)), {value: undefined, done: true});
+        assert.deepEqual(g.return(2), {value: 2        , done: true});
+        assert.deepEqual(g.return(4), {value: undefined, done: true});
+        assert.deepEqual(g.return(8), {value: undefined, done: true});
     });
 
     it('generator#throw(e: any)', () => {
