4d67489a8d9ac47b13ca0df7bce269fe068975c5
mizdra <pp.mizdra@gmail.com>
2016-09-04 21:59:11 +0900

    Fix generator

--- src/uint32/generator.ts
+++ src/uint32/generator.ts
@@ -6,22 +6,27 @@ import {u32} from './util';
 /**
  * The generator for LCG.
  * If maxFrame is 3, you can advance 3 frames.
- * @param lcgArg      - The argument of LCG
- * @param initialSeed - The first term of LCG
- * @param maxFrame    - The max frame to be able to be advanced
- * @returns           - A iterator
+ * @param lcgArg        - The argument of LCG, whose properties are treated as unsigned 32-bit integers
+ * @param initialSeed   - The first term of LCG, which is treated as unsigned 32-bit integers
+ * @param maxFrame      - The max frame to be able to be advanced
+ * @returns             - A iterator
+ * @throws {RangeError} - `maxFrame` must be non-NaN
  */
-export function* generator(lcgArg: ILCGArg, initialSeed: number, maxFrame: number = Infinity): IterableIterator<ILCGResult> {
-    if (maxFrame < 0) throw new RangeError("Invalid maxFrame.");
+export function generator(lcgArg: ILCGArg, initialSeed: number, maxFrame: number = Infinity): IterableIterator<ILCGResult> {
+    if (Number.isNaN(maxFrame)) throw new RangeError("Invalid maxFrame.");
 
-    let index = 0;
-    let seed = initialSeed;
+    return (function* (): IterableIterator<ILCGResult> {
+        const multiplier = u32(lcgArg.multiplier);
+        const increment  = u32(lcgArg.increment);
+        let seed         = u32(initialSeed);
+        let index        = 0;
 
-    while (index < maxFrame) {
-        seed = u32(Math.imul(lcgArg.multiplier, seed) + lcgArg.increment);
-        index++;
-        yield {seed, index};
-    }
+        while (index < maxFrame) {
+            seed = u32(Math.imul(multiplier, seed) + increment);
+            index++;
+            yield {seed, index};
+        }
+    })();
 }
 
 /**
@@ -29,7 +34,7 @@ export function* generator(lcgArg: ILCGArg, initialSeed: number, maxFrame: numbe
  */
 export interface ILCGResult {
     /**
-     * The index of LCG, which is the distance from a initial seed.
+     * The index of LCG, which is the number of times that `generator#next()` is called'.
      * If IterableIterator#next() is called twice, index is 2.
      */
     index: number;
